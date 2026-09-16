d6fb24420364b0e3e6efe34d4bb3f4579ada2486
avetrova <avetrova@calpoly.edu>
2026-09-16 16:49:00 -0700

    :3

---
+++ Module/src/Part1/Point.java
@@ -0,0 +1,36 @@
+package Part1;
+
+public class Point {
+
+    private double x;
+    private double y;
+
+    // constructor
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // getters
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    // setters
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    // rotates
+    public Point rotate() {
+        return new Point(-y, x);
+    }
+}
